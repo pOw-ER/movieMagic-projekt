821aef4980c00f4db8a8e6947307ac6120296d35
Enes Yilmaz <enesyilmaz60@hotmail.com>
2021-04-09 20:28:03 +0200

    style

--- index.js
+++ index.js
@@ -1,55 +1,123 @@
+const { response } = require('express')
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 require('dotenv').config()
 const PORT = process.env.PORT || 3003
-const genres = require('./data/genres.json')
+// const genres = require('./data/genres.json')
 
-let myMovie = 1;
-// let { myMovie1 } = require('./public/main.js')
-// console.log(myMovie1)
+
+
 app.set('view engine', 'ejs')
 
 app.listen(PORT, () => {
   console.log(`listening at localhost ${PORT}`);
 })
-let genreType = "";
+
 app.use(express.static('public'))
-const getData = async (myMovie) => {
-  const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genreType}&api_key=${process.env.MY_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${myMovie}`)
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+let genreType = ``;
+let myPage = 1;
+
+const getData = async (genreType, myPage) => {
+  const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genreType}&api_key=${process.env.MY_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${myPage}`)
   const data = await response.json()
   return data
 }
+const getDetail = async (movie_id) => {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${process.env.MY_API_KEY}&language=en-US`)
+  const detailData = await response.json()
 
-// with_genres=28&
-getData()
+  return detailData
+}
+
+getData("", myPage)
   .then((data) => {
-    app.use('/movies', (req, res, next) => {
-      let changeGenre = () => {
-        genreType = "28"
-      }
-      changeGenre()
-      next()
-    })
     app.get('/', (req, res) => {
       res.redirect('/movies')
     })
     app.get("/movies", (req, res) => {
-      res.render("pages/index", { data, myMovie: myMovie })
+      let myPage = 1;
+      res.render("pages/index", { data: data.results, myPage })
     })
 
     app.get("/movies/:id", (req, res) => {
-      let movie = data.results.find(elt => Number(req.params.id) == Number(elt.id))
-      // console.log(movie);
-      res.render("pages/movieDetails", { movie: movie, genres })
+      getDetail(req.params.id)
+        .then((data) => {
+          console.log(data);
+          res.render("pages/movieDetails", { movie: data })
+        })
+
     })
-    app.get("/movies/api/:i", (req, res) => {
-      console.log(req.params.i, 'hier ist sean');
-      myMovie = req.params.i
-      getData(myMovie)
-      res.render("/pages/index")
+    app.get("/movies/api/:id", (req, res) => {
+      let myPage = req.params.id
+      getData("", myPage)
+        .then((data) => {
+          res.render("pages/index", { data: data.results, myPage })
+        })
+
     })
 
+    ///// GENRES /////////
+    app.get("/movies/genres/action", (req, res) => {
+      let myPage = 1
+      getData("28", myPage)
+        .then((data) => {
+          res.render("pages/index", { data: data.results, myPage })
+        })
+    })
+    // app.get("/movies/genres/action/:id", (req, res) => {
+    //   getDetail(req.params.id)
+    //     .then((data) => {
+    //       res.render("pages/movieDetails", { movie: data })
+    //     })
+
+    // })
+    app.get("/movies/genres/drama", (req, res) => {
+      let myPage = 1
+      getData("18", myPage)
+        .then((data) => {
+          res.render("pages/index", { data: data.results, myPage })
+        })
+    })
+    app.get("/movies/genres/comedy", (req, res) => {
+      let myPage = 1
+      getData("35", myPage)
+        .then((data) => {
+          res.render("pages/index", { data: data.results, myPage })
+        })
+    })
+    app.get("/movies/genres/crime", (req, res) => {
+      let myPage = 1
+      getData("80", myPage)
+        .then((data) => {
+          res.render("pages/index", { data: data.results, myPage })
+        })
+    })
+    app.get("/movies/genres/horror", (req, res) => {
+      let myPage = 1
+      getData("27", myPage)
+        .then((data) => {
+          res.render("pages/index", { data: data.results, myPage })
+        })
+    })
+    app.get("/movies/genres/science_fiction", (req, res) => {
+      let myPage = 1
+      getData("878", myPage)
+        .then((data) => {
+          res.render("pages/index", { data: data.results, myPage })
+        })
+    })
+    app.get("/movies/genres/thriller", (req, res) => {
+      let myPage = 1
+      getData("53", myPage)
+        .then((data) => {
+          res.render("pages/index", { data: data.results, myPage })
+        })
+    })
   })
 
 
